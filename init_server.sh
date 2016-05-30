da81933d27992f8cd417d36528d9470cc3ff7165
abiao <abiao.chen@gmail.com>
2016-05-30 15:46:38 +0800

    added log example

--- init_server.sh
+++ init_server.sh
@@ -2,15 +2,18 @@
 # disable and overwrite
 echo "SELINUX=disabled" > /etc/selinux/config
 
-echo never > /sys/kernel/mm/redhat_transparent_hugepage/defrag
+#echo never > /sys/kernel/mm/redhat_transparent_hugepage/defrag
+echo never > /sys/kernel/mm/transparent_hugepage/defrag
 
 chkconfig iptables off
-/etc/init.d/network restart
-init 6
+setenforce 0
 
 echo "vm.swappiness=0" >> /etc/sysctl.conf
 
 yum install ntp
 service ntpd start
 chkconfig ntpd on
+
+/etc/init.d/network restart
+init 6
 
